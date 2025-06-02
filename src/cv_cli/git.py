037ae814302876bc4228c7d7cd8113cdbcd58599
Daniel Castro <dcastro@ualberta.ca>
2025-06-02 16:59:16 -0600

    chore: Added git function docs.

--- src/cv_cli/git.py
+++ src/cv_cli/git.py
@@ -4,14 +4,29 @@ from pathlib import Path
 from subprocess import run, CalledProcessError
 from shutil import rmtree
 
-def get_repo_url(repo_name):
+def get_repo_url(repo_name:str) -> None:
+    """Retrieves url of the specified repo.
+
+    Args:
+        repo_name (str): Name of git repo.
+
+    Raises:
+        FileNotFoundError: Repo does not exists.
+    """
     result = run(["gh", "repo", "view", repo_name, "--json", "url", "--jq", ".url"], capture_output=True, text=True)
     if result.returncode == 0:
         url = result.stdout.strip()
         return url
     raise FileNotFoundError("Repo could not be found.")
 
-def git_init(repo_dir:Path|str, repo_name, public:bool):
+def git_init(repo_dir:Path|str, repo_name:str, public:bool) -> None:
+    """Initialize git repo.
+
+    Args:
+        repo_dir (Path | str): Repo directory.
+        repo_name (str): Name of repo.
+        public (bool): Repo public/private flag.
+    """
     repo_type = "--public" if public else "--private"
     if not repo_dir.exists():
         repo_dir.mkdir(parents=True)
@@ -36,16 +51,33 @@ def git_init(repo_dir:Path|str, repo_name, public:bool):
 
     git_sync(repo_dir)
 
-def git_clone(remote_url:str, repo_dir:Path|str, force:bool):
+def git_clone(remote_url:str, repo_dir:Path|str, force:bool) -> None:
+    """Clones git repo.
+
+    Args:
+        remote_url (str): URL of source repo.
+        repo_dir (Path | str): Repo directory.
+        force (bool): Flag to force clone (Deletes pre-existing dir).
+    """
     if force:
         rmtree(repo_dir, onerror=onDelError)
     run(["git", "clone", remote_url, repo_dir], check=True)
 
-def git_pull(repo_dir):
+def git_pull(repo_dir:Path|str) -> None:
+    """Updates from git remote.
+
+    Args:
+        repo_dir (Path|str): Repo directory.
+    """
     run(["git", "branch", "-M", "main"], cwd=repo_dir, check=True)
     run(["git", "pull", "origin", "main"], cwd=repo_dir, check=True)
 
-def git_push(repo_dir):
+def git_push(repo_dir:Path|str) -> None:
+    """Pushes changes to git remote.
+
+    Args:
+        repo_dir (Path|str): Repo directory.
+    """
     run(["git", "branch", "-M", "main"], cwd=repo_dir, check=True)
     run(["git", "add", "."], cwd=repo_dir, check=True)
     try:
@@ -55,6 +87,11 @@ def git_push(repo_dir):
             print("[Error]: Commit failed.")
     run(["git", "push", "-u", "origin", "main"], cwd=repo_dir, check=True)
 
-def git_sync(repo_dir):
+def git_sync(repo_dir:Path|str) -> None:
+    """Syncs git repo with remote.
+
+    Args:
+        repo_dir (Path|str): Repo directory.
+    """
     git_push(repo_dir)
     git_pull(repo_dir)
